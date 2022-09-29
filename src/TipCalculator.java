794c2af4af36f785e06c2e32cb3dc7e1b76880db
ArhanH_____ <arhanh@nycstudents.net_____>
2022-09-29 18:34:29 -0400

    Completed

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class TipCalculator {
     public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    DecimalFormat formatter = new DecimalFormat("#.##");
 
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your group? ");
@@ -30,11 +32,21 @@ public class TipCalculator {
         System.out.println("------------------------");
         System.out.println("Total bill before tip: " + bill);
         System.out.println("Tip percentage: " + tipPercentage);
-        System.out.println("Total tip:" + (bill * INTPERCENTTODECIMAL));
-        System.out.println("Total bill with tip: " + (bill + (INTPERCENTTODECIMAL * bill)));
-        System.out.println("Per person cost before tip: " + (bill / members));
-        System.out.println("Tip per person: " + (INTPERCENTTODECIMAL * bill) / members);
-        System.out.println("Total cost per person:" + (bill + (INTPERCENTTODECIMAL * bill)) / members);
+        double totalTip = bill * INTPERCENTTODECIMAL;
+        String formattedtotalTip = formatter.format(totalTip);
+        System.out.println("Total tip: " + formatter.format(totalTip));
+        double tipWithBill = (bill + (INTPERCENTTODECIMAL * bill));
+        String formattedtipwithBill = formatter.format(tipWithBill);
+        System.out.println("Total bill with tip: " + formatter.format(tipWithBill));
+        double perPersonCost = bill / members;
+        String formattedperPersonCost = formatter.format(perPersonCost);
+        System.out.println("Per person cost before tip: " + formatter.format(perPersonCost));
+        double tipPerPerson = (INTPERCENTTODECIMAL * bill) / members;
+        String formattedtipPerPerson = formatter.format(tipPerPerson);
+        System.out.println("Tip per person: " + formatter.format(tipPerPerson));
+        double totalPeople = (bill + (INTPERCENTTODECIMAL * bill)) / members;
+        String formattedtotalPeople = formatter.format(totalPeople);
+        System.out.println("Total cost per person: " + formatter.format(totalPeople));
 
 
     }
